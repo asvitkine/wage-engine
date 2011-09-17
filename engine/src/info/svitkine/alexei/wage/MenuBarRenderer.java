3e47a0eaf635455d2f9608f296401581acc41653
alexei.svitkine <alexei.svitkine@24806d28-85d3-11de-8b65-cf205d531c0e>
2011-09-17 14:11:21 +0000

    more menu rendering

--- engine/src/info/svitkine/alexei/wage/MenuBarRenderer.java
+++ engine/src/info/svitkine/alexei/wage/MenuBarRenderer.java
@@ -10,14 +10,16 @@ import java.awt.event.MouseListener;
 import javax.swing.JComponent;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MenuBarRenderer extends JComponent implements MouseListener {
 	private static final int HEIGHT = 19;
+	private static final int PADDING = 6;
 
 	private JMenuBar menubar;
 	private int[] offsets;
 	private int[] spans;
-	private boolean[] pressed;
+	private int pressedMenu;
 	
 	public MenuBarRenderer(JMenuBar menubar) {
 		this.menubar = menubar;
@@ -26,7 +28,6 @@ public class MenuBarRenderer extends JComponent implements MouseListener {
 		FontMetrics m = getFontMetrics(f);
 		addMouseListener(this);
 		int menus = menubar.getMenuCount();
-		pressed = new boolean[menus];
 		offsets = new int[menus];
 		spans = new int[offsets.length];
 		int x = 20;
@@ -36,6 +37,7 @@ public class MenuBarRenderer extends JComponent implements MouseListener {
 			offsets[i] = x;
 			x += spans[i] + 12;
 		}
+		pressedMenu = -1;
 	}
 
 	@Override
@@ -46,14 +48,43 @@ public class MenuBarRenderer extends JComponent implements MouseListener {
 		g.fillRect(0, HEIGHT, getWidth(), 1);
 		Font f = getFont();
 		g.setFont(f);
+		// TODO: generalize this... have each 'menu' have bounds and a styled string...
 		for (int i = 0; i < menubar.getMenuCount(); i++) {
 			JMenu menu = menubar.getMenu(i);
 			g.setColor(Color.BLACK);
-			if (pressed[i]) {
+			if (pressedMenu == i) {
 				g.fillRect(offsets[i] - 6, 1, spans[i] + 12, HEIGHT - 1);
 				g.setColor(Color.WHITE);
 			}
 			g.drawString(menu.getText(), offsets[i], 14);
+			if (pressedMenu == i) {
+				int maxWidth = 0;
+				FontMetrics m = getFontMetrics(f);
+				for (int j = 0; j < menu.getItemCount(); j++) {
+					JMenuItem item = menu.getItem(j);
+					if (item != null) {
+						int width = m.stringWidth(item.getText());
+						if (width > maxWidth) {
+							maxWidth = width;
+						}
+					}
+				}
+				int x = offsets[i] - PADDING;
+				int y = HEIGHT;
+				int w = maxWidth + PADDING * 2;
+				int h = menu.getItemCount() * 20 + PADDING;
+				g.fillRect(x, y, w, h);
+				g.setColor(Color.BLACK);
+				g.drawRect(x, y, w, h);
+				y = 36;
+				for (int j = 0; j < menu.getItemCount(); j++) {
+					JMenuItem item = menu.getItem(j);
+					if (item != null) {
+						g.drawString(item.getText(), offsets[i], y);
+					}
+					y += 20;
+				}
+			}
 		}
 	}
 
@@ -62,7 +93,10 @@ public class MenuBarRenderer extends JComponent implements MouseListener {
 			return;
 		for (int i = 0; i < menubar.getMenuCount(); i++) {
 			if (event.getX() > offsets[i] - 6 && event.getX() - offsets[i] < spans[i] + 6) {
-				pressed[i] = !pressed[i];
+				if (pressedMenu == i)
+					pressedMenu = -1;
+				else
+					pressedMenu = i;
 				repaint();
 				break;
 			}
