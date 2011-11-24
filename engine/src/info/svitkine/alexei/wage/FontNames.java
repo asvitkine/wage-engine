f4402c370ab712b3863094a5e4c9798a5a9f4bf3
alexei.svitkine <alexei.svitkine@24806d28-85d3-11de-8b65-cf205d531c0e>
2011-11-24 23:31:11 +0000

    merge more complete font names list from: https://github.com/rgov/wage/commit/221196293a19e13b91f9fc8cce43ff915c6f2082

--- engine/src/info/svitkine/alexei/wage/FontNames.java
+++ engine/src/info/svitkine/alexei/wage/FontNames.java
@@ -1,52 +1,33 @@
 package info.svitkine.alexei.wage;
 
 public class FontNames {
+	// Source: Apple IIGS Technical Note #41, "Font Family Numbers"
+	// http://apple2.boldt.ca/?page=til/tn.iigs.041
 	private static final String[] FONTS = {
-			"Chicago",	// system font
-			"Geneva",	// application font
-			"New York",
-			"Geneva",
-
-			"Monaco",
-			"Venice",
-			"London",
-			"Athens",
+		"Chicago", // system font
+		"Geneva",  // application font
+		"New York",
+		"Geneva",
+		"Monaco",
+		"Venice",
+		"London",
+		"Athens",
+		"San Francisco",
+		"Toronto",
+		"Cairo",
+		"Los Angeles",
+		"Zapf Dingbats",
+		"Bookman",
+		"Helvetica Narrow",
+		"Palatino",
+		"Zapf Chancery",
+		"Times",
+		"Helvetica",
+		"Courier",
+		"Symbol",
+		"Taliesin"
+	};
 	
-			"San Francisco",
-			"Toronto",
-			"Cairo",
-			"Los Angeles", // 12
-
-			null, null, null, null, null, null, null, // not in Inside Macintosh
-
-			"Times", // 20
-			"Helvetica",
-			"Courier",
-			"Symbol",
-			"Taliesin" // mobile?
-		};
-
-	/*
-	mappings found on some forums:
-	systemFont(0):System(Swiss)
-	times(20):Times New Roman(Roman)
-	helvetica(21):Arial(Modern)
-	courier(22):Courier New(Modern)
-	symbol(23):Symbol(Decorative)
-	applFont(1):Arial(Swiss)
-	newYork(2):Times New Roman(Roman)
-	geneva(3):Arial(Swiss)
-	monaco(4):Courier New(Modern)
-	venice(5):Times New Roman(Roman)
-	london(6):Times New Roman(Roman)
-	athens(7):Times New Roman(Roman)
-	sanFran(8):Times New Roman(Roman)
-	toronto(9):Times New Roman(Roman)
-	cairo(11):Wingdings(Decorative)
-	losAngeles(12):Times New Roman(Roman)
-	taliesin(24):Wingdings(Decorative)
-	*/
-
 	public static String getFontName(int fontType) {
 		if (fontType >= 0 && fontType < FONTS.length && FONTS[fontType] != null) {
 			return FONTS[fontType];
